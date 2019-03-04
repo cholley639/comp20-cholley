e3355bf93e72980ea421671f637496a276296958
Cam Holley <cameron.holley@tufts.edu>
2019-03-04 11:28:40 -0500

    move js to seperate file 'script.js'

---
+++ liftknockoff/script.js
@@ -0,0 +1,80 @@
+//initializing map
+//Javascript for index.html in Assignment 2 comp20
+
+
+var myLat = 0
+var myLng = 0;
+var me = new google.maps.LatLng(42.4037887, 71.1142046);
+
+var myOptions = {
+    zoom: 13, // The larger the zoom number, the bigger the zoom
+    center: me,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map
+var marker;
+var infowindow = new google.maps.InfoWindow();
+
+function init() {
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    getMyLocation();
+}
+
+function getMyLocation() {
+    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+        navigator.geolocation.getCurrentPosition(function(position) {
+        myLat = position.coords.latitude;
+        myLng = position.coords.longitude;
+        initMap();
+        });
+    }
+    else {
+        alert("Geolocation is not supported by your web browser.  What a shame!");
+    }
+}
+
+function initMap() {
+
+    me = new google.maps.LatLng(myLat, myLng);
+    // Update map and go there...
+    map.panTo(me);
+
+    // Create a marker
+    marker = new google.maps.Marker({
+        position: me,
+        title: "Here I Am!"
+    });
+    marker.setMap(map);
+  
+    // Open info window on click of marker
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(marker.title);
+        infowindow.open(map, marker);
+    });
+    xmlrequests();
+}
+//xml request
+function xmlrequests() {
+
+    var username = "Tkwu74WC";
+    var param_string = "username=" + username +"&lat=" + myLat + "&lng=" + myLng;
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://hans-moleman.herokuapp.com/rides", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    console.log(request.readyState);
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            parse(request);
+        }
+    };
+    request.send(param_string);
+}
+
+function parse(request) {
+    objects = JSON.parse(request.responseText);
+    console.log(objects);
+    
+}
